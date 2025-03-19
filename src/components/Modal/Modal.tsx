d5d305f7fa6bd175e99d49ed158a088d620960f2
Field-devs <info@fieldcorp.com.br>
2025-03-19 09:13:01 -0300

    fixmodal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -28,28 +28,20 @@ export function ModalForm({ isOpen, onClose, children }: ModalProps) {
   return (
     <div
       className="fixed inset-0 bg-black/50 flex items-center justify-center z-50"
-      onMouseDown={async (e) => {
-        if (e.target === e.currentTarget) {
-          // let response = await AskDialog('Deseja realmente fechar o Formulário?', 'Fechar');
-          // if (response.value === true) onClose();
-          // onClose();
-        }
-      }}
+      onMouseDown={(e) => e.target === e.currentTarget && onClose()}
       role="dialog"
       aria-modal="true"
     >
       <div
-        className="relative w-full max-w-lg bg-white flex flex-col"
-        style={{
-          overflowY: 'auto', // Permite rolagem vertical
-          overflowX: 'hidden', // Impede rolagem horizontal
-          maxHeight: '100vh', // Limita a altura ao tamanho da janela
-        }}
+        className="relative w-full max-w-lg bg-white dark:bg-gray-800 flex flex-col overflow-y-auto scrollbar-none overflow-x-hidden max-h-[90vh] rounded-t-xl rounded-b-lg shadow-xl border border-gray-200 dark:border-gray-700 px-6 pt-6 pb-3 after:content-[''] after:absolute after:bottom-0 after:left-0 after:right-0 after:h-12 after:bg-gradient-to-t after:from-white/80 dark:after:from-gray-800/80 after:to-transparent after:pointer-events-none"
         onClick={(e) => e.stopPropagation()}
       >
-        {/* Botão de Fechar no canto superior direito */}
-        {children}
-        <XCloseForm onClose={onClose} />
+        <div className="space-y-6">
+          {children}
+          <div className="flex justify-end">
+            <XCloseForm onClose={onClose} />
+          </div>
+        </div>
       </div>
     </div>
   );
