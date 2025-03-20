a81d94ebdf3c5f7e4add38009b9dc271ecffab78
dixonmarinho <85615406+dixonmarinho@users.noreply.github.com>
2025-03-20 14:50:25 -0300

    Selecte Plan Show Inactive in Proposal Correct

--- src/pages/Forms/Proposal.Form.Plano.tsx
+++ src/pages/Forms/Proposal.Form.Plano.tsx
@@ -164,7 +164,7 @@ export default function ProposalFormPlano({ proposta, setProposta }: { proposta:
                   </option>
                 ))}
 
-                {plans.filter(plan => plan.active == viewInactive).map((plan) => (
+                {plans.filter(plan => plan.active == !viewInactive).map((plan) => (
                   <option key={plan.id} value={plan.id}>
                     {plan.nome}
                   </option>
